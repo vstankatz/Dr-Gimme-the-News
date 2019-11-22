67ddd85b0813d6e061f2ebed006e184f13446148
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-22 10:13:13 -0800

    add html outline

--- _tests_/querySearch.js
+++ _tests_/querySearch.js
@@ -3,8 +3,5 @@ import {QuerySearch} from './../src/doctor.js';
 describe('QuerySearch', () => {
 
 
-  test('should take in inputs into the fetch link', () => {
-    let search = new QuerySearch("John", "dental", "toothache", "male", "full-name-asc");
-    search.findDoctor();
-  })
+  
 })
--- src/doctor.js
+++ src/doctor.js
@@ -10,7 +10,11 @@ export class QuerySearch {
 
   async findDoctor(docName, docHelp, docSpec, docSex, docSort) {
     try {
-      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}%20K&query=${docHelp}&specialty_uid=${docSpec}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&gender=${docSex}&sort=${docSort}&skip=0&limit=20&user_key=${process.env.API_KEY}`)
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}%20K&query=${docHelp}&specialty_uid=${docSpec}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&gender=${docSex}&sort=${docSort}&skip=0&limit=20&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.log("There was an error handling your request: " + error.message);
     }
   }
 }
