0dfa8b74be4139a82691e6852bc29404f0bfa618
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-22 22:08:52 -0800

    add second API call for specialties

--- src/doctor.js
+++ src/doctor.js
@@ -1,3 +1,17 @@
+export class Specialist {
+
+  async findSpecialty() {
+    try {
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/specialties?user_key=${process.env.API_KEY}`);
+        let jsonifiedResponse = await response.json();
+        return jsonifiedResponse;
+    }catch(error) {
+      console.log("There was an error handling your request: " + error.message);
+    }
+  }
+}
+
+
 export class QuerySearch {
   constructor(docName, docSpec, docHelp, docSex, docSort) {
     this.docName = docName;
@@ -8,8 +22,9 @@ export class QuerySearch {
 
   }
 
+
+
   async findDoctor(docName, docHelp, docSpec, docSex, docSort) {
-    console.log(`${process.env.API_KEY}`);
     try {
       let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&query=${docHelp}&specialty_uid=${docSpec}&user_location=45.516022%2C-122.681427${docSex}${docSort}&skip=0&limit=20&user_key=${process.env.API_KEY}&location=45.516022%2C-122.681427%2C25`);
       let jsonifiedResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -1,19 +1,10 @@
-import {QuerySearch} from './doctor.js';
+import {Specialist, QuerySearch} from './doctor.js';
 import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-
-// function displayDoctors(doctorArrayItem) {
-//   let docList = $("ul#docList");
-//   let htmlForDocInfo = "";
-//
-//   doctorArrayItem.forEach(function(doctor) {
-//     htmlForDocInfo += '<li>' + doctor.name + doctor.city + doctor.spec + doctor.address + doctor.phone + doctor.website + "</li><hr>"
-//   })
-// };
 
 function translateInput(input) {
   const getComma = input.replace(",", "%2C")
@@ -25,41 +16,115 @@ function translateInput(input) {
 
 
 $(document).ready(function() {
+  let specQuery = new Specialist();
+      $('select#docSpec').change(function() {
+        let specList = $("select#specList")
+        let htmlForSpecInfo = "";
+        specList.empty();
+    if($(this).val()==="medical") {
+      $(".specList").show();
+      (async function () {
+      let response = await specQuery.findSpecialty();
+      response.data.forEach(function(option) {
+        if (option.category === "medical") {
+          htmlForSpecInfo += "<option value='" + option.uid + "'>" + option.name + "</option>"
+        }
+        specList.html(htmlForSpecInfo)
+      })
+    })()
+  } else if ($(this).val()==="dental") {
+    $(".specList").show();
+    (async function () {
+    let response = await specQuery.findSpecialty();
+    response.data.forEach(function(option) {
+      if (option.category === "dental") {
+        htmlForSpecInfo += "<option value='" + option.uid + "'>" + option.name + "</option>"
+      }
+      specList.html(htmlForSpecInfo)
+    })
+  })()
+} else if ($(this).val()==="vision") {
+  $(".specList").show();
+  (async function () {
+  let response = await specQuery.findSpecialty();
+  response.data.forEach(function(option) {
+    if (option.category === "vision") {
+      htmlForSpecInfo += "<option value='" + option.uid + "'>" + option.name + "</option>"
+    }
+    specList.html(htmlForSpecInfo)
+  })
+})()
+} else if ($(this).val()==="therapy") {
+  $(".specList").show();
+  (async function () {
+  let response = await specQuery.findSpecialty();
+  response.data.forEach(function(option) {
+    if (option.category === "therapy") {
+      htmlForSpecInfo += "<option value='" + option.uid + "'>" + option.name + "</option>"
+    }
+    specList.html(htmlForSpecInfo)
+  })
+})()
+} else if ($(this).val()==="services") {
+  $(".specList").show();
+  (async function () {
+  let response = await specQuery.findSpecialty();
+  response.data.forEach(function(option) {
+    if (option.category === "services") {
+      htmlForSpecInfo += "<option value='" + option.uid + "'>" + option.name + "</option>"
+    }
+    specList.html(htmlForSpecInfo)
+  })
+})()
+} else {
+  $(".specList").hide();
+
+  let newSpec = "";
+}
+
+})
+
+
   $("form#searchDoctor").submit(function(event) {
     event.preventDefault();
     let inputName = $('input#docName').val();
-    console.log(inputName);
-    let inputSpec = $('input#docSpec').val();
+    let inputSpec = $('select#specList').val();
     let inputHelp = $('input#docHelp').val();
     let newSex = $('input:radio[name=gender]:checked').val();
     let newSort = $('select#docSort').val();
     let docList = $("ul#docList");
     let htmlForDocInfo = "";
 
+
+
+
     (async function () {
       let undefinedVar = /undefined/gi;
       let newName = translateInput(inputName);
-      let newSpec = translateInput(inputSpec);
       let newHelp = translateInput(inputHelp);
-              console.log(`${process.env.API_KEY}`);
+      let newSpec
+        if (inputSpec === null) {
+         newSpec = "";
+        } else {
+          newSpec = inputSpec;
+        }
       const search = new QuerySearch(newName, newSpec, newHelp, newSex, newSort);
       let response = await search.findDoctor(newName, newSpec, newHelp, newSex, newSort);
 
+      console.log(newSpec);
+      console.log(search);
       response.data.forEach(function(doctor) {
 
         htmlForDocInfo += "<hr> <li> <img class='docPic' src=" +  doctor.profile.image_url + " " + "'></li>" +
         '<li>' + "Doctor " + doctor.profile.first_name + " " + doctor.profile.middle_name + " " + doctor.profile.last_name + ", " + doctor.profile.title + ".</li><br>"
 
-        console.log(doctor.profile.image_url);
 
         doctor.practices.forEach(function(location) {
 
-          console.log(location.phones[0].number);
           htmlForDocInfo += "<ul><li>Address: " + location.visit_address.street + " " + location.visit_address.city + ", " + location.visit_address.state + " " + location.visit_address.zip +  "<br>Currently accepting new patients: " + location.accepts_new_patients + "<br>" + location.phones[0].type + " number: " + location.phones[0].number + ".</li></ul><br>"
         })
 
         doctor.specialties.forEach(function(specialty) {
-          console.log(specialty.name);
           htmlForDocInfo += "<li>Specialty: " +  specialty.name
         })
 
@@ -69,11 +134,8 @@ $(document).ready(function() {
 
       });
 
-      // displayDoctors(search);
+
     })();
-
-
-
 
 
 
